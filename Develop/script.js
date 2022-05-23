7b18317f619eb257ebdfd2b59b87445e3626fbe9
Julian Heaney <julianpheaney@gmail.com>
2022-05-22 20:52:40 -0500

    updated in app text

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 var lowerCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
-var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; 
-var specialCharacters = ["!", "#", "$", "%", "&", "'", "*", "(", ")"]; 
+var numericCharacters = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]; 
+var specialCharacters = ["!", "#", "$", "%", "&", "'", "*", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "}", "|", "~"]; 
 
 
 var generateBtn = document.querySelector("#generate");
@@ -23,31 +23,31 @@ function generatePassword() {
   randomPW = []; 
 
 
-  var pwLength = parseInt(prompt("Enter a password length between 8 and 128 characters:"));
+  var pwLength = parseInt(prompt("Enter a length for your password between 8 and 128 characters:"));
 
  
   if (pwLength < 8 || pwLength > 128 || !pwLength) {
-    alert("Oops! Try again"); 
+    alert("Password length selected is either too short or too long. Try again"); 
       generatePassword(); 
   } 
 
 
-    var lowerPW = confirm("Click OK to INCLUDE LowerCase Characters in your Password. Click Cancel to Exclude.");
+    var lowerPW = confirm("Click OK to include LowerCase Characters. Click Cancel to Exclude.");
       if(lowerPW) {
       chosenChars = chosenChars.concat(lowerCharacters); 
       }
 
-    var upperPW = confirm("Click OK to INCLUDE UpperCase Characters in your Password. Click Cancel to Exclude."); 
+    var upperPW = confirm("Click OK to include UpperCase Characters. Click Cancel to Exclude."); 
       if(upperPW) {
       chosenChars = chosenChars.concat(upperCharacters);
       }
 
-    var numericPW = confirm("Click OK to INCLUDE Numeric Characters in your Password. Click Cancel to Exclude."); 
+    var numericPW = confirm("Click OK to include Numeric Characters. Click Cancel to Exclude."); 
       if(numericPW){
       chosenChars = chosenChars.concat(numericCharacters);
       }
 
-    var specialPW = confirm("Click OK to INCLUDE Special Characters in your Password. Click Cancel to Exclude."); 
+    var specialPW = confirm("Click OK to include Special Characters. Click Cancel to Exclude."); 
       if(specialPW){
       chosenChars = chosenChars.concat(specialCharacters);
       }
